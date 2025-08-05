882ebe67bdc00a24092f656f952d9843d08a6815
NeishaPutrie <neishaputrie4@gmail.com>
2025-08-05 19:30:38 +0700

    Add files via upload

---
+++ BelanjaBarengNeisha.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class BelanjaBarengNeisha {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Nama Barang 1");
+        String namaBarang1 = input.next();
+        System.out.println("Harga Barang 1");
+        double harga1 = input.nextDouble();
+        System.out.println("Masukan Nama Barang 2");
+        String namaBarang2 = input.next();
+        System.out.println("Harga Barang 2");
+        double harga2 = input.nextDouble();
+
+        System.out.println("=====Daftar Belanja=====");
+        System.out.println("Nama Barang 1: "+namaBarang1);
+        System.out.println("Harga Barang 1: "+harga1);
+        System.out.println("-------------------");
+        System.out.println("Nama Barang 2: "+namaBarang2);
+        System.out.println("Harga Barang 2: "+harga2);
+        System.out.println("========================");
+    }
+
+}
